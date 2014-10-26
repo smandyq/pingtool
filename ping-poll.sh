9eee2536afdb633142ab6706918fa14a1b1ec6a8
Smandy <smandy@quirky.com>
2014-10-26 03:15:07 -0700

    working on new url schema

--- ping-poll.sh
+++ ping-poll.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-endpoint="https://pingtool.firebaseio.com/pings.json"
-
 host=$(tracepath google.com -b -m 2 | grep '^ 2\:.*' | sed 's/.*(\([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\)).*/\1/')
 localhost=$(hostname)
 
-echo using host $host
+hlocalhost=${localhost//'.'/'-'}
+hhost=${host//'.'/'-'}
+
+endpoint="https://pingtool.firebaseio.com/pings/$hlocalhost/$hhost.json"
+
+echo $endpoint
 
 while read -r line; do
   pingtime=$(echo $line | sed 's/.*time=\([0-9.]\+\) ms/\1/' | grep -e '^[0-9.]\+$')
